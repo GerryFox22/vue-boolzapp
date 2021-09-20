5594d9427dc11c1a7639695c225601733b1b8d16
GerryFox22 <gerry.cuomo1994@gmail.com>
2021-09-20 11:27:54 +0200

    implementazione e termine Milestone 2

--- js/myscript.js
+++ js/myscript.js
@@ -104,17 +104,39 @@ var app = new Vue({
         
         contattoSelezionato : function(index){
             this.segnaElemento = index;
-        }
+        },
 
+    
     },
 
   });
 
 
-// Il Milestone 1 include esclusivamente:
-// Nota Bene: non è possibile in nessun caso modificare i dati nell'oggetto contacts da noi fornito.
+// Il Milestone 1 include esclusivamente: OK!
 
 // 1. Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) 
 //    e dall’interlocutore (bianco) assegnando due classi CSS diverse
 
 // 2. Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto.
+
+
+// Il Milestone 2 include esclusivamente: OK!
+
+// Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti 
+// i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// Click sul contatto mostra la conversazione del contatto cliccato
+
+
+// Il Milestone 3 include esclusivamente:
+
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” 
+// il testo viene aggiunto al thread sopra, come messaggio verde
+// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come 
+// risposta, che apparirà dopo 1 secondo.
+
+
+// Il Milestone 4 include esclusivamente:
+
+// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo 
+// i contatti il cui nome contiene le lettere inserite 
+// (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
